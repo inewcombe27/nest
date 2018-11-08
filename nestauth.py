a221abea4c60a21290272ac716400509f1c2d464
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-07 21:43:23 -0500

    ahhh

--- nestauth.py
+++ nestauth.py
@@ -3,7 +3,7 @@ import os
 
 conn = http.client.HTTPSConnection("api.home.nest.com")
 
-payload = "code =" + os.environ['AUTH_CODE'] + \
+payload = "code=" + os.environ['AUTH_CODE'] + \
     "&client_id=" + os.environ['PRODUCT_ID']+"&client_secret=" \
     + os.environ['PRODUCT_SECRET'] + "&grant_type=authorization_code"
 
