8cd3635dbacb1ca8b67b39124cf4eba09dfdba1b
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-07 21:42:38 -0500

    now should be good

--- nestauth.py
+++ nestauth.py
@@ -4,8 +4,8 @@ import os
 conn = http.client.HTTPSConnection("api.home.nest.com")
 
 payload = "code =" + os.environ['AUTH_CODE'] + \
-    "&client_id=" + os.environ['CLIENT_ID']+"&client_secret=" \
-    + os.environ['client_secret'] + "&grant_type=authorization_code"
+    "&client_id=" + os.environ['PRODUCT_ID']+"&client_secret=" \
+    + os.environ['PRODUCT_SECRET'] + "&grant_type=authorization_code"
 
 print (payload)
 
