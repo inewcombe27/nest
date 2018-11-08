ac20de17cce7f8821264e248746958e216e50cac
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-07 21:05:46 -0500

    auth add

---
+++ nestauth.py
@@ -0,0 +1,14 @@
+import http.client
+
+conn = http.client.HTTPSConnection("api.home.nest.com")
+
+payload = "code=AUTH_CODE&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&grant_type=authorization_code"
+
+headers = { 'content-type': "application/x-www-form-urlencoded" }
+
+conn.request("POST", "/oauth2/access_token", payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
