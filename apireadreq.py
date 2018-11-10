1aa9caba03539ee9c468f39c482b51292d20e5de
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 13:02:50 -0500

    daf

--- apireadreq.py
+++ apireadreq.py
@@ -1,17 +1,18 @@
 import requests
 import os
+import json
 # from urllib.parse import urlparse
 
 
 token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
-        'Content-Type': 'application/json'}  # Update with your token
+         'Content-Type': 'application/json'}  # Update with your token
 
 
-r = requests.get('https://developer-api.nest.com/', headers=token,
-                allow_redirects=False)
+resp = requests.get('https://developer-api.nest.com/', headers=token,
+                    allow_redirects=False)
 
-data = r.text
-print (data)
+data = resp.text
+print resp.json()
 # if r.status == 307:
 #     redirectLocation = urlparse(response.getheader("location"))
 #     conn = http.client.HTTPSConnection(redirectLocation.netloc)
