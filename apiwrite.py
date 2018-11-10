5e84b1c78d911a8470eef01767aa0d2cd6d0ae79
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 13:46:02 -0500

    add write

---
+++ apiwrite.py
@@ -0,0 +1,17 @@
+import requests
+import os
+import config
+
+url = "https://developer-api.nest.com/devices/thermostats/" + config.down_id
+
+token = config.auth_token# Update with your token
+
+payload = "{\"target_temp_f\": \"78"}"
+
+headers = {'Authorization': 'Bearer {0}'.format(token), 'Content-Type': 'application/json'}
+
+response = requests.put(url, headers=headers, data=payload, allow_redirects=False)
+if response.status_code == 307: # indicates a redirect is needed
+    response = requests.put(response.headers['Location'], headers=headers, data=payload, allow_redirects=False)
+
+print(response.text)
