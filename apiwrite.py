e756633a90b39090e0a66ec2f12e93acfe68b4d8
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 14:53:19 -0500

    cealnup

--- apiwrite.py
+++ apiwrite.py
@@ -9,13 +9,12 @@ def thermo_write(dev_id, tparm, tvalue):
 
     # payload = "{'\\' tparm: tvalue '\\'}"
     payload = "{\"" + tparm + "\""":" + str(tvalue) + "}"
-    print (payload)
+
     headers = {'Authorization': 'Bearer {0}'.format(token),
                'Content-Type': 'application/json'}
 
     response = requests.put(url, headers=headers, data=payload,
                             allow_redirects=False)
-    print (response.url)
 
     if response.status_code == 307:  # indicates a redirect is needed
         response = requests.put(response.headers['Location'],
