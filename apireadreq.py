03ad2cd7a574141099581628184f995f61b798c0
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 12:38:56 -0500

    changed params to headers

--- apireadreq.py
+++ apireadreq.py
@@ -6,7 +6,7 @@ import os
 token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
         'Content-Type': 'application/json'}  # Update with your token
 
-r = requests.get('https://developer-api.nest.com', params=token)
+r = requests.get('https://developer-api.nest.com', headers=token)
 
 
 # if r.status == 307:
