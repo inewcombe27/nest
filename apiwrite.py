9ea67351e836ff43ca95915be85e1e3d151f8782
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 14:15:15 -0500

    try again

--- apiwrite.py
+++ apiwrite.py
@@ -8,7 +8,7 @@ def thermo_write(dev_id, tparm, tvalue):
     token = config.auth_token  # Update with your token
 
     # payload = "{'\\' tparm: tvalue '\\'}"
-    payload = "{\"temperature_scale\": \"F\"}"
+    payload = "{\"tparm\": \"tvalue\"}"
 
     headers = {'Authorization': 'Bearer {0}'.format(token),
                'Content-Type': 'application/json'}
