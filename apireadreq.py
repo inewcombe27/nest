d846ea5c64e4f6ed2b91f8353ff34cfc1450b382
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 12:50:34 -0500

    print some things

--- apireadreq.py
+++ apireadreq.py
@@ -6,6 +6,8 @@ import os
 token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
         'Content-Type': 'application/json'}  # Update with your token
 
+print (token)
+
 r = requests.get('https://developer-api.nest.com/', headers=token, allow_redirects=False)
 
 
