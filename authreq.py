342861889af764fac8533530d2992167c9547048
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 11:47:06 -0500

    fixes

--- authreq.py
+++ authreq.py
@@ -1,15 +1,13 @@
 import requests
 import os
 
-conn = http.client.HTTPSConnection("api.home.nest.com")
-
 payload = {'code': os.environ['AUTH_CODE'],
         'client_id': os.environ['PRODUCT_ID'], 'client_secret': os.environ
         ['PRODUCT_SECRET'], 'grant_type': 'authorization_code'}
 
 print (payload)
 
-auth = requests.post('https://api.home.nest.com/oauth2/access_token', \
-    params = payload)
+auth = requests.post('https://api.home.nest.com/oauth2/access_token',
+        params=payload)
 
 print (r.url)
