3805982f49b8c9ef30ae5b03c695dda75c5ad0b5
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 13:09:03 -0500

    fasdf

--- apireadreq.py
+++ apireadreq.py
@@ -7,7 +7,7 @@ import json
 
 url = "https://developer-api.nest.com/"
 
-token = os.environ['AUTH_TOKEN']# Update with your token
+token = os.environ['AUTH_TOKEN']  # Update with your token
 
 headers = {'Authorization': 'Bearer {0}'.format(token), 'Content-Type': 'application/json'}
 
@@ -16,6 +16,7 @@ response = requests.get(url, headers=headers, allow_redirects=False)
 if response.status_code == 307:
     response = requests.get(response.headers['Location'], headers=headers, allow_redirects=False)
 
+print (response.text)
 # token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
 #          'Content-Type': 'application/json'}  # Update with your token
 #
@@ -24,7 +25,7 @@ if response.status_code == 307:
 #                     allow_redirects=False)
 #
 # data = resp.text
-print (response.text)
+
 # if r.status == 307:
 #     redirectLocation = urlparse(response.getheader("location"))
 #     conn = http.client.HTTPSConnection(redirectLocation.netloc)
