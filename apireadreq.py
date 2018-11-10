a82035fc6e81f85918799ac69c8fe73d497e2277
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 13:04:24 -0500

    add nest code

--- apireadreq.py
+++ apireadreq.py
@@ -4,15 +4,24 @@ import json
 # from urllib.parse import urlparse
 
 
-token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
-         'Content-Type': 'application/json'}  # Update with your token
 
+url = "https://developer-api.nest.com/"
 
-resp = requests.get('https://developer-api.nest.com/', headers=token,
-                    allow_redirects=False)
+token = os.environ['AUTH_TOKEN']# Update with your token
 
-data = resp.text
-print resp.json()
+headers = {'Authorization': 'Bearer {0}'.format(token), 'Content-Type': 'application/json'}
+
+response = requests.get(url, headers=headers, allow_redirects=False)
+
+# token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
+#          'Content-Type': 'application/json'}  # Update with your token
+#
+#
+# resp = requests.get('https://developer-api.nest.com/', headers=token,
+#                     allow_redirects=False)
+#
+# data = resp.text
+print response.json()
 # if r.status == 307:
 #     redirectLocation = urlparse(response.getheader("location"))
 #     conn = http.client.HTTPSConnection(redirectLocation.netloc)
