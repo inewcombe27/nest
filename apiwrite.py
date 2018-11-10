97e18e47c842d4350b3498b1272cf173509865cf
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 14:38:40 -0500

    int to str

--- apiwrite.py
+++ apiwrite.py
@@ -8,7 +8,7 @@ def thermo_write(dev_id, tparm, tvalue):
     token = config.auth_token  # Update with your token
 
     # payload = "{'\\' tparm: tvalue '\\'}"
-    payload = "{" + tparm + ":" + tvalue + "}"
+    payload = "{" + tparm + ":" + str(tvalue) + "}"
 
 
 
