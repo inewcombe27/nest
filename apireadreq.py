470d099d21ea1b0d76e07374501e0fd433f12e0c
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 13:15:26 -0500

    delete all nest.com code

--- apireadreq.py
+++ apireadreq.py
@@ -1,22 +1,7 @@
 import requests
 import os
 import json
-# from urllib.parse import urlparse
 
-
-
-# url = "https://developer-api.nest.com/"
-#
-# token = os.environ['AUTH_TOKEN']  # Update with your token
-#
-# headers = {'Authorization': 'Bearer {0}'.format(token), 'Content-Type': 'application/json'}
-#
-# response = requests.get(url, headers=headers, allow_redirects=False)
-#
-# if response.status_code == 307:
-#     response = requests.get(response.headers['Location'], headers=headers, allow_redirects=False)
-
-# print (response.text)
 token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
          'Content-Type': 'application/json'}  # Update with your token
 
@@ -28,13 +13,3 @@ if response.status_code == 307:
      response = requests.get(response.headers['Location'], headers=token, allow_redirects=False)
 
 print (response.text)
-
-# if r.status == 307:
-#     redirectLocation = urlparse(response.getheader("location"))
-#     conn = http.client.HTTPSConnection(redirectLocation.netloc)
-#     conn.request("GET", "/", headers=headers)
-#     response = conn.getresponse()
-#     if response.status != 200:
-#         raise Exception("Redirect with non 200 response")
-
-# data = r.text
