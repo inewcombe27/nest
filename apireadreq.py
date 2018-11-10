fb72dde57cb86b35b5570cc163aedbe7db0a3717
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 14:55:28 -0500

    cealnup

--- apireadreq.py
+++ apireadreq.py
@@ -1,5 +1,4 @@
 import requests
-import os
 import config
 
 token = {'Authorization': 'Bearer {0}'.format(config.auth_token),
@@ -9,8 +8,7 @@ token = {'Authorization': 'Bearer {0}'.format(config.auth_token),
 response = requests.get('https://developer-api.nest.com/', headers=token,
                     allow_redirects=False)
 
-if response.status_code == 307:
-     response = requests.get(response.headers['Location'], headers=token, allow_redirects=False)
+if response.status_code == 307:response = requests.get(response.headers['Location'], headers=token, allow_redirects=False)
 
 data = response.json()
 print (data['target_temperature_f'])
