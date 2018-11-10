137d2f5b16918b1ef6e6b7c1807b8ae2a27f84a5
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 12:37:52 -0500

    add content type

--- apireadreq.py
+++ apireadreq.py
@@ -3,7 +3,8 @@ import os
 # from urllib.parse import urlparse
 
 
-token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN'])}  # Update with your token
+token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
+        'Content-Type': 'application/json'}  # Update with your token
 
 r = requests.get('https://developer-api.nest.com', params=token)
 
