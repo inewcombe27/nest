5bdf38430e4ad4130febbc129b94be6122a05c6d
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 12:07:47 -0500

    new read

---
+++ apireadreq.py
@@ -0,0 +1,19 @@
+import requests
+from urllib.parse import urlparse
+import nestauth
+
+token = {'authorization': nestauth.authr["access_token"]}  # Update with your token
+
+r = requests.get('https://developer-api.nest.com', params=token)
+
+
+# if r.status == 307:
+#     redirectLocation = urlparse(response.getheader("location"))
+#     conn = http.client.HTTPSConnection(redirectLocation.netloc)
+#     conn.request("GET", "/", headers=headers)
+#     response = conn.getresponse()
+#     if response.status != 200:
+#         raise Exception("Redirect with non 200 response")
+
+data = r.text
+print(data)
