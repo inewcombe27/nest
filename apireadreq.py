875f078cce188c35d535d812fe4253c653796d7b
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-11-10 13:12:38 -0500

    try my code again

--- apireadreq.py
+++ apireadreq.py
@@ -5,26 +5,29 @@ import json
 
 
 
-url = "https://developer-api.nest.com/"
+# url = "https://developer-api.nest.com/"
+#
+# token = os.environ['AUTH_TOKEN']  # Update with your token
+#
+# headers = {'Authorization': 'Bearer {0}'.format(token), 'Content-Type': 'application/json'}
+#
+# response = requests.get(url, headers=headers, allow_redirects=False)
+#
+# if response.status_code == 307:
+#     response = requests.get(response.headers['Location'], headers=headers, allow_redirects=False)
 
-token = os.environ['AUTH_TOKEN']  # Update with your token
+# print (response.text)
+token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
+         'Content-Type': 'application/json'}  # Update with your token
 
-headers = {'Authorization': 'Bearer {0}'.format(token), 'Content-Type': 'application/json'}
 
-response = requests.get(url, headers=headers, allow_redirects=False)
+resp = requests.get('https://developer-api.nest.com/', headers=token,
+                    allow_redirects=False)
 
 if response.status_code == 307:
-    response = requests.get(response.headers['Location'], headers=headers, allow_redirects=False)
+     response = requests.get(response.headers['Location'], headers=headers, allow_redirects=False)
 
 print (response.text)
-# token = {'Authorization': 'Bearer {0}'.format(os.environ['AUTH_TOKEN']),
-#          'Content-Type': 'application/json'}  # Update with your token
-#
-#
-# resp = requests.get('https://developer-api.nest.com/', headers=token,
-#                     allow_redirects=False)
-#
-# data = resp.text
 
 # if r.status == 307:
 #     redirectLocation = urlparse(response.getheader("location"))
